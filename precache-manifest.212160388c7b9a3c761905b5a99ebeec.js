29210b2cd91ea93d121dfc6a219679bdd369c93e
sudipto baral <sudiptobaral.me@gmail.com>
2020-03-07 22:50:19 +0600

    Fist project commit

---
+++ precache-manifest.212160388c7b9a3c761905b5a99ebeec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018dcc3323f53b5ec168043dce720fb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d7964485d984499bc43",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "744e42d75108dc2146b4",
+    "url": "./static/js/2.ffd532a9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.ffd532a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7964485d984499bc43",
+    "url": "./static/js/main.8ed02665.chunk.js"
+  },
+  {
+    "revision": "ec3c74850e2114d512fd",
+    "url": "./static/js/runtime-main.4a4c5629.js"
+  }
+]);
