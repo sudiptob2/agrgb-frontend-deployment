abeee0a7203d38e84ee1c760342c2cec11740ba4
sudipto baral <sudiptobaral.me@gmail.com>
2020-03-08 12:27:13 +0600

    Version 2

---
+++ precache-manifest.991e8900315f2f9618cb1e969fab64a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9edbc72f8d33294060b25ac8805376e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77eb63a789bf6b97f1a8",
+    "url": "./static/css/main.a5e8ed89.chunk.css"
+  },
+  {
+    "revision": "d31d7276ead430d7b554",
+    "url": "./static/js/2.475fb377.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "./static/js/2.475fb377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77eb63a789bf6b97f1a8",
+    "url": "./static/js/main.e5069219.chunk.js"
+  },
+  {
+    "revision": "ec3c74850e2114d512fd",
+    "url": "./static/js/runtime-main.4a4c5629.js"
+  },
+  {
+    "revision": "87d7828578b1ca7fd4616b3902edddeb",
+    "url": "./static/media/Logo.87d78285.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21ecee912977442f16ec80931edb844e.js"
+  "./precache-manifest.991e8900315f2f9618cb1e969fab64a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
